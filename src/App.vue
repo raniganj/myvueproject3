71734f1ac39d4f79d4d06c3c469b79e841dc3d2e
sunilmondal4you <sunilmondal4you@gmail.com>
2023-07-25 13:15:12 +0530

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import MyStaticText from './components/MyStaticText.vue'
+import MyNavbar from './components/MyNavbar.vue'
 </script>
 
 <template>
+  <MyNavbar />
   <MyStaticText />
 </template>
