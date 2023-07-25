f27b3fa5a4444e116592203ead9e754c9efe5834
sunilmondal4you <sunilmondal4you@gmail.com>
2023-07-25 19:32:01 +0530

    state management

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <script setup>
+import { provide, ref } from 'vue'
 import MyNavbar from './components/MyNavbar.vue'
-import MyListApi from './components/MyListApi.vue'
+import MyStateComponent from './components/MyStateComponent.vue'
+import MyStateComponent2 from './components/MyStateComponent2.vue'
+
+let counter = ref(10)
+let incementCounter = () => {
+  counter.value++
+}
+
+provide('counter', { counter, incementCounter })
 </script>
 
 <template>
   <MyNavbar />
-  <MyListApi />
+  <MyStateComponent />
+  <MyStateComponent2 />
 </template>
