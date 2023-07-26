40c9b05b66c000feaa91d1590ce68cc471147df7
sunilmondal4you <sunilmondal4you@gmail.com>
2023-07-26 10:04:09 +0530

    axios add

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,9 @@
 <script setup>
-import { provide, ref } from 'vue'
 import MyNavbar from './components/MyNavbar.vue'
-import MyStateComponent from './components/MyStateComponent.vue'
-import MyStateComponent2 from './components/MyStateComponent2.vue'
-import MyStateComponent3 from './components/MyStateComponent3.vue'
-
-let counter = ref(10)
-let incementCounter = () => {
-  counter.value++
-}
-
-provide('counter', { counter, incementCounter })
+import MyAxiosApi from './components/MyAxiosApi.vue'
 </script>
 
 <template>
   <MyNavbar />
-  <MyStateComponent />
-  <MyStateComponent2 />
-  <MyStateComponent3 />
+  <MyAxiosApi />
 </template>
