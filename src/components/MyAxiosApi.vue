a3a60b07b6d4426f2f89cbcfe47488d4daf3a34d
sunilmondal4you <sunilmondal4you@gmail.com>
2023-07-26 10:06:35 +0530

    fetch methode

--- src/components/MyAxiosApi.vue
+++ src/components/MyAxiosApi.vue
@@ -1,15 +1,19 @@
 <script setup>
 import { ref } from 'vue'
 
-let post = ref({})
+let post = ref()
+
 let fetchPost = async () => {
   let url = 'https://jsonplaceholder.typicode.com/posts/1'
   let response = await fetch(url)
   post.value = await response.json()
 }
 </script>
+
 <template>
   <h1>Data using Fetch</h1>
+
   <button class="btn btn-primary" @click="fetchPost()">Fetch</button>
-  <h3>Title : {{ post?.title }}</h3>
+
+  <h3 v-if="post">Title : {{ post?.title }}</h3>
 </template>
